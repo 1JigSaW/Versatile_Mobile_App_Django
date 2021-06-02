d53a9800909dd0bf375e32d69d87bd87b03f8ab4
JigSaW <misho_1998@mail.ru>
2021-06-03 01:57:47 +0300

    add entries

--- pizzaproject/stores/models.py
+++ pizzaproject/stores/models.py
@@ -5,10 +5,11 @@ class Pizzeria(models.Model):
 	pizzeria_name = models.CharField(max_length=200, blank=False)
 	street = models.CharField(max_length=400, blank=True)
 	city = models.CharField(max_length=400, blank=True)
-	state = models.CharFiels(max_length=2, null=True, blank=True)
+	state = models.CharField(max_length=2, null=True, blank=True)
 	zip_code = models.IntegerField(blank=True, default=0)
 	website = models.URLField(max_length=420, blank=True)
-	phone_number = models.CharField(validators=[RegexValidator(regex=r'^\1?\d{9,10}$')],
+	phone_number = models.CharField(
+		validators=[RegexValidator(regex=r'^\d{9,10}$')],
 		max_length=10,
 		blank=True)
 	description = models.TextField(blank=True)
@@ -17,5 +18,8 @@ class Pizzeria(models.Model):
 		default="pizzariaImages/pizzalogo.png")
 	email = models.EmailField(max_length=245, blank=True)
 	active = models.BooleanField(default=True)
+	
+	def __str__(self):
+		return "{}, {}".format(self.pizzeria_name, self.city)
 	
 	
