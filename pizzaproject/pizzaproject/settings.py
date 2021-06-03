3553f98028f36f2bfa39a920a25930264daf0e4b
JigSaW <misho_1998@mail.ru>
2021-06-04 01:22:14 +0300

    restful components

--- pizzaproject/pizzaproject/settings.py
+++ pizzaproject/pizzaproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-@hhrj*e4ltyauze(5pi%h9n^$==rxq4j6iixuanw(4n3#6bpym'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- pizzaproject/stores/views.py
+++ pizzaproject/stores/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import PizzeriaListSerializer
-from .model import Pizzeria
+from .serializers import PizzeriaListSerializer, PizzeriaDetailSerializer
+from .models import Pizzeria
 
 class PizzeriaListAPIView(generics.ListAPIView):
 	queryset = Pizzeria.objects.all()
 	serializer_class = PizzeriaListSerializer	
+
+class PizzeriaRetrieveAPIView(generics.RetrieveAPIView):
+	lookup_field = 'id'
+	queryset = Pizzeria.objects.all()
+	serializer_class = PizzeriaDetailSerializer
+
+class PizzeriaCreateAPIView(generics.CreateAPIView):
+	queryset = Pizzeria.objects.all()
+	serializer_class = PizzeriaDetailSerializer
+
+class PizzeriaRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+	lookup_field = 'id'
+	queryset = Pizzeria.objects.all()
+	serializer_class = PizzeriaDetailSerializer
+
+class PizzeriaDestroyAPIView(generics.DestroyAPIView):
+	lookup_field = 'id'
+	queryset = Pizzeria.objects.all()
