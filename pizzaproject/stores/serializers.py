38bea63dd99360ad65ae8dbfdec53fc235428f16
JigSaW <misho_1998@mail.ru>
2021-06-05 02:02:19 +0300

    Add forms and search

--- pizzaproject/stores/serializers.py
+++ pizzaproject/stores/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
 from .models import Pizzeria
+from rest_framework.reverse import reverse
 
 class PizzeriaListSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Pizzeria
-		fields = [
-			'id',
-			'pizzeria_name',
-			'city',
-			'zip_code',
-		]
+	
+    absolute_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Pizzeria
+        fields = [
+            'id',
+            'pizzeria_name',
+            'city',
+            'zip_code',
+            'absolute_url'
+        ]
+
+    def get_absolute_url(self, obj):
+        return reverse('pizzeria_detail', args=(obj.pk,))
+
 class PizzeriaDetailSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Pizzeria
